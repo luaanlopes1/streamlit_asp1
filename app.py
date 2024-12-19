88a77c1d703e14186ce67750780a06d32d14090b
Luan Lopes <114255328+luaanlopes1@users.noreply.github.com>
2024-12-18 23:53:10 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -42,28 +42,28 @@ def extrair_e_salvar_arquivos_pdf_pagina(pdf_bytes):
     return resultados_por_pagina
 
 
-# Streamlit App
-st.title("Processador de Arquivos PDF")
-st.write("Faça upload de um arquivo PDF para extrair informações e gerar arquivos TXT.")
 
-# Upload do PDF
+st.title("Processador de PDF Listagem de Documentos GedAcervo")
+st.write("Faça upload de um arquivo PDF de algum serviço para extrair informações dos nomes dos 'arquivos.pdf' e em seguida, sera gerado um arquivo txt para copiar e colar no aplicativo 'WinSCP'.")
+st.write('1º Passo: Entrar na pasta devida do serviço dentro da AWS onde sera ira pegar os PDFs')
+st.write('2º Passo: Utilizar ctrl + alt + f')
+st.write('3º Passo: Ira abrir uma janela então você clica em EDITAR')
+st.write('4º Passo: Copie tudo do arquivo txt gerado e cole dentro de: *Incluir Arquivos* e em seguida OK e depois mais uma vez clicar em OK. ')
+st.write('5º Conferir se o número de arquivos filtrados batem com o numero de registros encontrados no PDF')
+st.write('6º Dar um ctrl+a e arrastar pro lado esquerdo do WinSCP para baixar todos os documentos filtrados do PDF.')
+
 uploaded_pdf = st.file_uploader("Envie seu arquivo PDF", type=["pdf"])
 
 if uploaded_pdf is not None:
-    # Botão para processar
     if st.button("Iniciar processamento"):
-        # Processar o PDF
         resultados, contador = extrair_e_salvar_arquivos_pdf(uploaded_pdf)
         resultados_por_pagina = extrair_e_salvar_arquivos_pdf_pagina(uploaded_pdf)
-
-        # Criar os arquivos TXT em memória
+        
         txt_todos = "\n".join(resultados)
         txt_por_pagina = "\n".join(resultados_por_pagina)
 
-        # Exibir mensagem de sucesso
         st.success(f"Processamento concluído! Foram encontrados {contador} arquivos.")
 
-        # Botões de download
         st.download_button(
             label="Baixar resultados (todos juntos)",
             data=txt_todos,
